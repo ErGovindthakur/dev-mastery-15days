92c80109c8e25017f5aa350d1447329045a91995
ErGovindthakur <ergovindthakur@gmail.com>
2026-05-08 18:39:09 +0530

    implement middleware code

--- day-06-MernRedisDocker/server/src/routes/todoRoutes.js
+++ day-06-MernRedisDocker/server/src/routes/todoRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { createTodo, deleteTodo, getAllTodos, getSingleTodo, UpdateTodo } from "../controllers/todoController.js";
+import { authenticatedUser } from "../middleware/authMiddleware.js";
 
 const route = express.Router();
 
-route.post("/createTodo",createTodo);
+route.post("/createTodo",authenticatedUser,createTodo);
 route.get("/getAllTodos",getAllTodos);
 route.get("/getSingleTodo/:id",getSingleTodo);
-route.put("/updateTodo/:id",UpdateTodo);
-route.delete("/deleteTodo/:id",deleteTodo);
+route.put("/updateTodo/:id",authenticatedUser,UpdateTodo);
+route.delete("/deleteTodo/:id",authenticatedUser,deleteTodo);
 
 
 export const TodoRoute = route;
